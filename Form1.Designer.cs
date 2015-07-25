3995cf65e8959768888b0cf78eddf7b1ff4d487f
M4nuski <the_man994@hotmail.com>
2015-07-25 04:11:57 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace LUA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 358);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(690, 330);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "x = x + 0.1\r\ny = y + 0.3";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(16, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update Script";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(690, 311);
+            this.panel1.TabIndex = 3;
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 700);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "LUA OpenGL brain fucking test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Windows.Forms;
+using NLua.Exceptions;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform;
+
+using NLua;
+
+
+namespace LUA
+{
+    public partial class Form1 : Form
+    {
+        public IWindowInfo WindowInfo;
+        public GraphicsContext WindowContext;
+
+        public Lua state = new Lua();
+        public string script;
+        public int cnt;
+        public double x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Closing += Form1_Closing;
+
+            WindowInfo = Utilities.CreateWindowsWindowInfo(panel1.Handle);
+            var WindowMode = new GraphicsMode(32, 24, 0, 0, 0, 2);
+            WindowContext = new GraphicsContext(WindowMode, WindowInfo, 2, 0, GraphicsContextFlags.Debug);
+
+            WindowContext.MakeCurrent(WindowInfo);
+            WindowContext.LoadAll(); // as IGraphicsContextInternal)
+
+            WindowContext.SwapInterval = 1;
+            GL.Viewport(0, 0, panel1.Width, panel1.Height);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.Disable(EnableCap.CullFace);
+            GL.ClearColor(Color.DarkBlue);
+
+            state["x"] = 0.0;
+            state["y"] = 1.0;
+            state["fn"] = 0;
+            script = textBox1.Text;
+
+            timer1.Enabled = true;
+        }
+
+        public void Form1_Closing(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            script = textBox1.Text;
+            timer1.Enabled = true;
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            WindowContext.MakeCurrent(WindowInfo);
+        //    GL.Viewport(0, 0, panel1.Width, panel1.Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit |
+                     ClearBufferMask.DepthBufferBit);
+
+            cnt++;
+            state["fn"] = cnt;
+            label1.Text = string.Format("fn={0} x={1} y={2}",cnt,x,y);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Frustum(-3.0d, 3.0d, -3.0d, 3.0d, 1.0d, 10.0d);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            try
+            {
+                state.DoString(script);
+
+            }
+            catch (LuaException ex)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show(ex.Message, "LUA Parser Exception", MessageBoxButtons.OK);
+            }
+            x = state.GetNumber("x");
+            y = state.GetNumber("y");
+
+            GL.Translate(0.0d, 0.0d, -5.0d);
+            GL.Scale(3,3,3);
+            GL.Rotate(x, 1.0f, 0.0f, 0.0f);
+            GL.Rotate(y, 0.0f, 1.0f, 0.0f);
+
+
+
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Color4(1.0f, 0.0f, 0.0f, 1.0f);
+            GL.Vertex3(0.0f, 1.0f, 0.0f);
+            GL.Color4(0.0f, 1.0f, 0.0f, 1.0f);
+            GL.Vertex3(-1.0f, 0.0f, 1.0f);
+            GL.Color4(0.0f, 0.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 0.0f, -1.0f);
+            GL.End();
+
+
+
+            WindowContext.SwapBuffers();
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            WindowContext.MakeCurrent(WindowInfo);
+            GL.Viewport(0, 0, panel1.Width, panel1.Height);
+
+        }
+    }
+}
